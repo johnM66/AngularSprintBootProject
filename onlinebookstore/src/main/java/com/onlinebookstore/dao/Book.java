37d61d7d8389e1d54baba6de9fa6f1701ab7921b
johnM66 <32862342+johnM66@users.noreply.github.com>
2021-05-14 21:16:13 +0530

    Create DTO Book.

---
+++ onlinebookstore/src/main/java/com/onlinebookstore/dao/Book.java
@@ -0,0 +1,41 @@
+package com.onlinebookstore.dao;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+
+@Table(name = "tbl_book")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String sku;
+	private String name;
+	private String description;
+	@Column(name = "unit_price")
+	private BigDecimal unitPrice;
+	@Column(name = "img_url")
+	private String imageUrl;
+	private boolean active;
+	@Column(name = "units_in_stock)")
+	private int unitsOfStock;
+	@Column(name = "date_created")
+	private Date createOn;
+	@Column(name = "last_updated")
+	private Date updateOn;
+}
